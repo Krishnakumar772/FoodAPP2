6f89370ce95668a2e850ac7756533bb75306f779
Krishna <krishnarathore6217@gmail.com>
2023-07-26 12:16:47 +0530

    Addedd new Feture

---
+++ src/COMPONENT/ChildProsp/Parent.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import Child from './Child'
+class Parent extends React.Component {
+
+	state = {
+		name: "",
+	}
+
+	// Callback function to handle data received from the
+	//child component
+	handleCallback = (childData) => {
+		// Update the name in the component's state
+		this.setState({ name: childData })
+	}
+
+	render() {
+		const { name } = this.state;
+		return (
+			<div>
+				<Child parentCallback={this.handleCallback} />
+				{name}
+			</div>
+		)
+	}
+}
+export default Parent
